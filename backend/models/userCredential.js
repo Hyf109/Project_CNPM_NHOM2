b6ed1629356a1dce209f04307c8c3facd57f1acb
Tran Thai Duong <22028061@vnu.edu.vn>
2024-05-09 18:03:37 +0700

    Hashing password

--- backend/models/userCredential.js
+++ backend/models/userCredential.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -21,6 +22,20 @@ const userSchema = new mongoose.Schema({
         unique: true
     }
 });
+
+//Call a function after a document is saved to database
+// userSchema.post('save', function(doc, next) {
+//     console.log('User saved account', doc);
+//     next();
+// })
+
+//Call a function before doc is saved:
+userSchema.pre('save', async function(next) {
+    //User normal function to use this operator to access local instance of use data object
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
 const User = mongoose.model('user', userSchema);
 
