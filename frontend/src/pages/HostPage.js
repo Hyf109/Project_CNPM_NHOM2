f032af8c0332827814fb6ca6a78bff8f45959b38
Cuong Do <cgdd2511.hd@gmail.com>
2024-05-16 14:45:07 +0700

    EventList and EventCard Updates

--- frontend/src/pages/HostPage.js
+++ frontend/src/pages/HostPage.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import Header from "components/HeaderOnly/Appbar";
-import EventListContainer from "components/HostEvent/EventList/EventListContainer";
+import EventListContainer from "components/EventList/EventListContainer";
 import EventCard from 'components/EventCard/EventCard';
 function HostEvent() {
     return (
         <>
-        <EventCard />
+        <EventListContainer/>
         
         </>
     )
