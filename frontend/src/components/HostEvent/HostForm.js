9f019d97ee2bf8320603e6918ed0c6c3429f082a
Cuong Do <cgdd2511.hd@gmail.com>
2024-05-17 18:50:14 +0700

    modifying

--- frontend/src/components/HostEvent/HostForm.js
+++ frontend/src/components/HostEvent/HostForm.js
@@ -11,17 +11,25 @@ function HostForm() {
             <div className="host-container">
                 <div className="host-form form-1"> 
                     <input type="text" className="text-box name-input" placeholder="Event name"></input>
+                </div>
+                <div className="host-form-2">
                     <div className="date-select">
-                        <input type="datetime-local" className="text-box date-input" placeholder="Start time"></input>
-                        <input type="datetime-local" className="text-box date-input" placeholder="End time"></input>
+                        <div className="start-time">
+                            <h1 className="form-2-name-tag">Start time</h1>
+                            <input type="datetime-local" className="text-box date-input" placeholder="Start time"></input>
+                        </div>
+                        <div className="end-time">
+                            <h1 className="form-2-name-tag">End time</h1>
+                            <input type="datetime-local" className="text-box date-input" placeholder="End time"></input>
+                        </div>
+                        
                     </div>
                 </div>
-                <div className="host-form form-2">   
-                    <Button className="button pick-button">Pick from map</Button>
+                <div className="host-form form-3">   
                     <input type="text" className="text-box place-input" placeholder="Place"></input>
                     <input type="text" className="text-box PartNum-input" placeholder="Participants Number"></input>
                 </div> 
-                <div className="host-form form-3">
+                <div className="host-form form-4">
                     <textarea type="text" className="text-box des-input" placeholder="Description"></textarea>  
                 </div>
                 <div className="host-form form-5">
