bbca97d72e2bd7bba2dd168f4f7a9221fe2f39b3
Tran Thai Duong <22028061@vnu.edu.vn>
2024-05-19 03:51:44 +0700

    User info forms and button

--- frontend/src/components/UserProfile/UserProfile/Description/Description.js
+++ frontend/src/components/UserProfile/UserProfile/Description/Description.js
@@ -2,13 +2,20 @@ import React from "react";
 import './Description.scss'
 import { Link } from "react-router-dom";
 import catAvt from 'components/assets/images/cat.png'
+import { useAuth } from "hooks/useAuth";
 
 function Description() {
+    const {user, isPending, error} = useAuth();
+    
+    if (isPending) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className="user-infomation">
             <span className="avatar">
                 <img src={catAvt}></img>
-                <b>Username</b>
+                <b>{user.username}</b>
                 <i className="fa-solid fa-user-pen"></i>
             </span>
         </div>
--- frontend/src/components/UserProfile/UserProfile/UserInfo/UserInfo.js
+++ frontend/src/components/UserProfile/UserProfile/UserInfo/UserInfo.js
@@ -1,11 +1,37 @@
 import React from "react";
 import './UserInfo.scss'
+import { useState } from "react";
 
 function UserInfo() {
+    const [editable, setEditable] = useState(false);
+
     return (
         <div className="user-info">
             <div className="user-info-title">
                 <h2>Information</h2>
+                <div className="info-buttons">
+                        {
+                            !editable && <button onClick={(e) => {
+                                e.preventDefault();
+                                setEditable(true);
+                            }} className="info-edit-button">Edit</button>
+                        }
+
+                        {
+                            editable && 
+                                <>
+                                    <button onClick={(e) => {
+                                        e.preventDefault();
+                                            setEditable(false);
+                                        }} className="info-save-button">Save</button>
+                                        
+                                        <button onClick={(e) => {
+                                            e.preventDefault();
+                                            setEditable(false);
+                                    }} className="info-cancel-button">Cancel</button>
+                                </>
+                        }
+                    </div>
             </div>
         </div>
     )
