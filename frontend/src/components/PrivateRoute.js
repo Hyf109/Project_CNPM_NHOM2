b710810b3ccae6ce3834ad4bc7100c14b799dc53
ghfu_thym <22028217@vnu.edu.vn>
2024-05-18 23:43:19 +0700

    add unit test

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -7,9 +7,9 @@ const PrivateRoute = () => {
 
     const {user} = useAuth();
 
-    if (!user) {
-        return <Navigate to="/signin" />;
-    }
+    // if (!user) {
+    //     return <Navigate to="/signin" />;
+    // }
 
     return <Outlet/>
 }
