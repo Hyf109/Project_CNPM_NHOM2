318325185c21c35ed7aa7f3fa279606f63da53c4
Tran Thai Duong <22028061@vnu.edu.vn>
2024-05-19 15:28:47 +0700

    Added login function

--- frontend/src/components/UserProfile/UserProfile/Description/Description.js
+++ frontend/src/components/UserProfile/UserProfile/Description/Description.js
@@ -1,15 +1,23 @@
 import React from "react";
 import './Description.scss';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import catAvt from 'components/assets/images/cat.png';
 import { useAuth } from "hooks/useAuth";
+import { useLogout } from "hooks/useLogout";
 
 function Description() {
-    const { user } = useAuth();
+    const { user, dispatch } = useAuth();
+    const {logout} = useLogout();
+    const navigate = useNavigate();
 
     // Ensure user is not null before accessing properties
     if (!user) {
         return <div>Loading...</div>;
+    }
+
+    const handleLogOut = () => {
+        logout();
+        navigate('/');
     }
 
     return (
@@ -18,6 +26,10 @@ function Description() {
                 <img src={catAvt} alt="User Avatar" />
                 <b>{user.username}</b>
                 <i className="fa-solid fa-user-pen"></i>
+            </span>
+
+            <span className="logout-button-container">
+                <button className="logout-button" onClick={handleLogOut}>Logout</button>
             </span>
         </div>
     );
